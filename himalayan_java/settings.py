8b88450049b524446674e2a2a8962545c84bd4b4
Ratish149 <ratish.shakya149@gmail.com>
2025-10-10 13:16:24 +0545

    host setting change

--- himalayan_java/settings.py
+++ himalayan_java/settings.py
@@ -117,10 +117,21 @@ WSGI_APPLICATION = "himalayan_java.wsgi.application"
 
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
+""" DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
         "NAME": BASE_DIR / "db.sqlite3",
+    }
+} """
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "himalayan_java",
+        "USER": "ratish",
+        "PASSWORD": "ratish123",
+        "HOST": "localhost",
+        "PORT": "",
     }
 }
 
